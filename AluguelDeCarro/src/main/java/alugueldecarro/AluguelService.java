1fcb6e08f6ef8b8f88c8350feab766dbffc66e91
igor.oliveira <igor.oliveira@E0101-002.fan.local>
2024-06-13 11:50:28 -0300

    Atualização 13-06

--- AluguelDeCarro/src/main/java/alugueldecarro/AluguelService.java
+++ AluguelDeCarro/src/main/java/alugueldecarro/AluguelService.java
@@ -11,9 +11,9 @@ public class AluguelService {
 
 	//Constructor
 	public AluguelService(List<Carro> carros, List<Cliente> clientes, List<Aluguel> alugueis) {
-		carros = new ArrayList<>();
-		clientes = new ArrayList<>();
-		alugueis = new ArrayList<>();
+		this.carros = new ArrayList<>();
+		this.clientes = new ArrayList<>();
+		this.alugueis = new ArrayList<>();
 	}
 	
 	public void addCarro(Carro carro) {
@@ -23,6 +23,10 @@ public class AluguelService {
 	public void addCliente(Cliente cliente) {
 		clientes.add(cliente);
 	}
+	
+	public List<Carro> listarCarros() {
+	        return carros;
+	} 
 	
 	//Metodo para alugar um carro
 	public void alugarCarro(Carro carro, Cliente cliente, int dias) {
--- AluguelDeCarro/src/main/java/alugueldecarro/Menu.java
+++ AluguelDeCarro/src/main/java/alugueldecarro/Menu.java
@@ -1,8 +1,55 @@
 package main.java.alugueldecarro;
 
+import java.util.List;
+import java.util.Scanner;
+
 public class Menu {
-	
-	public static void menu(String[] args) {
+
+	private static AluguelService aluguelservice;
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("==== Alugue seu carro ====");
+		System.out.println("1. Alugar um carro");
+		System.out.println("2. Devolver um carro");
+		System.out.println("3. Sair");
+		System.out.print("Escolha uma opção: ");
+
+		int opcao = scanner.nextInt();
+		scanner.nextLine();
+
+		switch (opcao) {
+		case 1:
+			alugarCarro(scanner);
+			break;
+		case 2:
+			devolverCarro(scanner);
+			break;
+		case 3:
+			System.out.println("Saindo do programa.");
+			break;
+		default:
+			System.out.println("Opção inválida.");
+		}
+		scanner.close();
+
+	}
+
+	private static void alugarCarro(Scanner scanner){
+		System.out.println("Alugar um carro");
+		List<Carro> carros = aluguelservice.listarCarros();
 		
+		if (carros.isEmpty()){
+			System.out.println("Não há carros disponíveis.");
+			return;
+		}
+		
+		System.out.println("\nCarros disponíveis: ");
+		for(int i = 0; i < carros.size(); i++) {
+			Carro carro = carros.get(i);
+		}
+
+
 	}
 }
